751a4842d5c0c602e341988d6d2da91562465ea2
Syed Izhaan S <izhaan9802@gmail.com>
2023-01-18 20:14:51 +0530

    Sign Up page Style

--- client/status-tracker/src/pages/SignUp.js
+++ client/status-tracker/src/pages/SignUp.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
+import s from "../styles/SignUp.module.css";
 
 const SignUp = () => {
     
@@ -29,26 +30,31 @@ const SignUp = () => {
     }
 
     return (<>
-        <form>
-            <label htmlFor="name">Enter Name</label>
-            <input type="text" name="name" onChange={e => {setName(e.target.value)}}/><br/>
-            <label htmlFor="rno">Enter Register No.</label>
-            <input type="text" name="rno" onChange={e => {setRegNo(e.target.value)}}/><br/>
-            <label htmlFor="email">Enter Email</label>
-            <input type="email" name="email" onChange={e => {setEmail(e.target.value)}}/><br/>
-            <label htmlFor="dept">Enter Department</label>
-            <select name="dept" id="dept" onChange={e => {setDept(e.target.value)}}>
+        <form className={s.form}>
+        <p className={s.loginText}>Sign Up</p>
+        <label className={s.enterYourCredentials}>Enter Your Credentials</label>
+            {/* <label htmlFor="name">Enter Name</label> */}
+            <input className={s.inputBox} type="text" placeholder="Name"name="name" onChange={e => {setName(e.target.value)}}/><br/>
+            {/* <label htmlFor="rno">Enter Register No.</label> */}
+            <input className={s.inputBox} type="text" placeholder="Register No" name="rno" onChange={e => {setRegNo(e.target.value)}}/><br/>
+            {/* <label htmlFor="email">Enter Email</label> */}
+            <input className={s.inputBox} type="email" placeholder="Email" name="email" onChange={e => {setEmail(e.target.value)}}/><br/>
+            {/* <label htmlFor="dept">Enter Department</label> */}
+            <select className={s.selectTags} name="dept" id="dept" onChange={e => {setDept(e.target.value)}}>
+                <option   value="" disabled selected>Department</option>
                 <option value="cse">CSE</option>
                 <option value="IT">IT</option>
             </select><br/>
-            <label htmlFor="tt">Training Type</label>
-            <select name="tt" id="tt" onChange={e => {setTrainType(e.target.value)}}>
+            {/* <label htmlFor="tt">Training Type</label> */}
+            <select className={s.selectTags} name="tt" id="tt" onChange={e => {setTrainType(e.target.value)}}>
+            <option color="black" value="" disabled selected>Training Type</option>
                 <option value="fullstack">Fullstack</option>
                 <option value="competitveprogramming">Competitive Programming</option>
             </select><br/>
-            <label htmlFor="password">Enter Password</label>
-            <input type="password" name="password" onChange={e => {setPassword(e.target.value)}}/><br/>
-            <button type="submit" onClick={formHandler}>Submit</button>
+            {/* <label htmlFor="password">Enter Password</label> */}
+            <input className={s.inputBox} placeholder="Password"type="password" name="password" onChange={e => {setPassword(e.target.value)}}/><br/>
+            <button className={s.submitButton} type="submit" onClick={formHandler}>Submit</button>
+            <p>Aldready have an Account? Login</p>
         </form>
     </>);
 }
